193484ac487661aa57e91460bf2159ec36366703
Ignacio <ignaciopalmeri1@gmail.comgit config --global user.email ignaciopalmeri1@gmail.com>
2025-09-06 12:27:52 -0300

    Agregue una funcion para que se pueda filtrar vviajes por origen y asi uso el lambda y filter

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ def anotarNuevoViaje (): #Esta funcion se usa cuando el usuario inicia un viaje 
 
 def menu (): #es el menu principal, que nos terminara llevando a cada parte del programa
     opcion= ""
-    while opcion != "4":
+    while opcion != "5":
         print ("Bienvenido a nuestro gestor de viajes")
         print ("1 Iniciar nuevo viaje")
         print ("2 Consulta tus viajes")
         print ("3 Elimar viaje")
-        print ("4 Salir del menu")
+        print ("4 Filtrar viaje por origen")
+        print ("5 Salir del menu")
         opcion= input ("Opcion: ")
 
         if opcion == "1":
@@ -27,6 +28,8 @@ def menu (): #es el menu principal, que nos terminara llevando a cada parte del 
         elif opcion== "3":
             print("Falta esta funcioon")
         elif opcion == "4":
+            filtrarPorOrigen()
+        elif opcion == "5":
             print("Salir")
         else:
             print("Error, opcion invalida")
@@ -41,3 +44,17 @@ def mostrarViajeExistente (): #unicamente imprime los viajes ya cargados, para p
         for i in range(len(viajes)):
             viaje = viajes[i]
             print(i + 1, "desde", viaje[0], "hasta", viaje[1], "fecha", viaje[2])
+
+def filtrarPorOrigen():  #esta funcion la usamos si el usuario desea filtrar los viajes existentes y encontrar por origen
+    if len(viajes) == 0:
+        print("No hay ningún viaje cargado")
+    else:
+        origen_buscado = input("Ingrese el origen a buscar: ")
+        viajes_filtrados = list(filter(lambda viaje: viaje.origen == origen_buscado, viajes))
+
+        if len(viajes_filtrados) == 0:
+            print("No hay viajes desde", origen_buscado)
+        else:
+            print("Viajes encontrados:")
+            for viaje in viajes_filtrados:
+                print(viaje)
