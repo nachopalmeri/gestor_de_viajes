400ddda065cecca3b3162cea27d289514017e1d1
lautaro <leavallejoss01@gmail.com>
2025-09-09 20:36:13 -0300

    agreggo funcion para ver la lista de pasajeros y si no hay mostrar que no hay pasajeros cargados.

--- ingreso_pasajeros/ingreso.py
+++ ingreso_pasajeros/ingreso.py
@@ -13,11 +13,23 @@ def ingreso_pasajeros():
         elif opcion == "2":
             lista_pasajeros(pasajeros)
         elif opcion == "3":
-            print("Saliendo...")
+            print("Saliendo.")
             break
         else:
             print("Opcion no valida.")
-
+    
+def lista_pasajeros(lista_pasajeros):
+    if len(lista_pasajeros) == 0:
+        print("No hay pasajeros cargados.")
+    else:
+        print("--- Lista de Pasajeros ---")
+        for i in range(len(lista_pasajeros)):
+            pasajero = lista_pasajeros[i]
+            print("Pasajero", i + 1)
+            print("Nombre:", pasajero["nombre"])
+            print("Dni:", pasajero["dni"])
+            print("Asiento:", pasajero["asiento"])
+            print("----------------------")       
 
 def cargar_pasajero(lista_pasajeros):
     nombre = input("nombre del pasajero: ")
@@ -31,7 +43,5 @@ def cargar_pasajero(lista_pasajeros):
     }
 
     lista_pasajeros.append(pasajero)
-    print("Pasajero agregado correctamente.")
-
-
+    print("Pasajero agregado correctamente.")            
 ingreso_pasajeros()
