766dc62b751f7eb3d2d5698f58107e1da3fb6902
lautaro <leavallejoss01@gmail.com>
2025-09-16 19:56:35 -0300

    agrego archivo donde va a estar todo el programa

--- principal.py
+++ principal.py
@@ -1,6 +1,30 @@
 import re
 viajes = []  # Aca guardamos todos los datos del viaje : ("origen":, "destino":, "fecha":, "asientos":, "pasajeros":)
 
+def anotarNuevoViaje (): #Esta funcion se usa cuando el usuario inicia un viaje por primera vez en el programa
+    print("Nuevo viaje? Ingreselo aca: ")
+    origen= input("Ingrese el origen: ")
+    destino= input("Ingrese el destino: ")
+    fecha=input( "Ingrese la fecha: ")
+    viaje= [origen,destino, fecha]
+    viajes.append(viaje)
+    print("Su viaje fue creado")
+
+
+
+def menu (): #es el menu principal, que nos terminara llevando a cada parte del programa
+    print ("Bienvenido a nuestro gestor de viajes")
+    print ("1 Iniciar nuevo viaje")
+    print ("2 Consulta tus viajes")
+    print ("3 Elimar vviaje")
+    print ("4 Salir del menu")
+    opcion= input ("Opcion: ")
+    
+    if opcion == "1":
+        anotarNuevoViaje()
+
+
+
 def matriz_asientos(filas, columnas):
     matriz = []
     for i in range(filas):
